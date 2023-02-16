afe127265c7626c6fa406aafecb794954d2ff2e7
Nathanael Suarez <nate.suarez@icloud.com>
2023-02-15 22:16:07 -0600

    Initial Commit

---
+++ Spotlight/SpotlightApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotlightApp.swift
+//  Spotlight
+//
+//  Created by Nathanael Suarez on 2/15/23.
+//
+
+import SwiftUI
+
+@main
+struct SpotlightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
